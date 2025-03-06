58f9ea0a035649dfe9569643c449bd6f45fc4889
Vedang <vedang.wappnet@gmail.com>
2025-03-06 10:05:37 +0530

    added the route for admin

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.post("/create", authenticateUser, orderController.createOrder);
 router.get('/vendor', authenticateUser, authorizeRole('vendor') ,orderController.getOrderedProductOfVendor);
+router.get('/admin', authenticateUser, authorizeRole('admin'), orderController.getAllOrders);
 router.get("/", authenticateUser, orderController.getAllOrdersByCustomerId);
 router.get("/:order_id", authenticateUser, orderController.getOrderItemsByOrderId);
-router.put("/:id", authenticateUser, orderController.updateStatusOfOrderItem);
+router.put("/:id", authenticateUser, authorizeRole('vendor'), orderController.updateStatusOfOrderItem);
 
 module.exports = router;
